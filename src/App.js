5075ca8d112885db538ae684e8eaf3dc0d958b08
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2017-05-15 18:23:05 -0300

    Update deps

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <section className={styles.app}>
           <div className={styles.header}>
             <img src={logo} className={ styles.logo } alt="logo" />
-            <h2>Welcome to Ballers</h2>
+            <h2>Welcome to Ballers!</h2>
           </div>
           <p className={styles.intro}>
             To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/header/README.md
@@ -0,0 +1,7 @@
+Header:
+
+<Header />
+
+If you define a fenced code block with a language flag it will be rendered as a regular Markdown code snippet:
+
+import React from 'react';
